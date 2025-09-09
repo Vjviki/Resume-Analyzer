cb55542fe918b05a0bd32521ab7922741942bc7e
vignesh <dinovicky1010@gmail.com>
2025-09-09 13:09:48 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Resume Analyzer Backend is running!");
+});
+
 // Routes
 app.use("/api/resumes", resumeRoutes);
 
